e11892c4c980bb11d141d3ec26a551db2ec75a29
velopace <velopace@outlook.com>
2023-06-08 11:05:45 -0400

    Ranking Grid

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,5 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
 
 const RankItems = () => {
     const [items, setItems] = useState([]);
@@ -16,7 +17,8 @@ const RankItems = () => {
     })
 
     return (
-        <main>
+        <main className="main">
+            <RankingGrid items={items} imgArr={MovieImageArr} />
             <div className="items-not-ranked">
                 {
                     (items.length > 0) ? items.map((item) =>
