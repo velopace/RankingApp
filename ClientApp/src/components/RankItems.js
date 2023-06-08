e9d81dd07c6b0783f4990caf53db489c29362115
velopace <velopace@outlook.com>
2023-06-08 12:07:18 -0400

    fix drag/drop

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -45,11 +45,12 @@ const RankItems = () => {
             <div className="items-not-ranked">
                 {
                     (items.length > 0) ? items.map((item) =>
-                        <div className="unranked-cell" key={item.id}>
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}
-                                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
-                            />
-                        </div>
+                        (item.ranking === 0) ?
+                            <div className="unranked-cell" key={item.id}>
+                                <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}
+                                    style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+                                />
+                            </div> : null
                     ) : <div>Loading...</div>
                 }
             </div>
