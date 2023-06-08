d5a0dee86cb18328e02329daa54f28c262dd8510
velopace <velopace@outlook.com>
2023-06-08 09:00:42 -0400

    add RankItems

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -37,7 +37,10 @@ export class NavMenu extends Component {
               </NavItem>
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/rank-items">Rank Items</NavLink>
+                </NavItem>
             </ul>
           </Collapse>
         </Navbar>
